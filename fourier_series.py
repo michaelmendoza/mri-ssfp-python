8cac2b7e78a42bd49d6fb9912cbcb16d6fdcfb98
Michael Mendoza <m.mendoza711@gmail.com>
2017-11-03 16:55:45 -0600

    Refactored fourier_series

--- fourier_series.py
+++ fourier_series.py
@@ -5,36 +5,43 @@ import math
 import matplotlib.pyplot as plt 
 import matplotlib.image as mpimg
 
-N = 1000
-x = np.linspace(0, 10, N)
-T = 2
-L = T / 2 
-
-def f(x): 
-    a = np.sin((np.pi) * x)
-    a = np.maximum(a, np.zeros(N))
-    return a
-
-def a(n, L):
+''' Odd Coefficents '''
+def a(x, L, n):
     a, b = -L, L
+    N = len(x)
     dx = (b - a) / N
     integration = np.sum(f(x) * np.cos((n * np.pi * x) / L)) * dx
     return (1 / L) * integration
 
-def b(n, L):
+''' Even Coefficents '''
+def b(x, L, n):
     a, b = -L, L
+    N = len(x)
     dx = (b - a) / N
     integration = np.sum(f(x) * np.sin((n * np.pi * x) / L)) * dx
     return (1 / L) * integration
 
-# Fourier series 
+''' Fourier series '''
 def Sf(x, L, n = 10):
-    a0 = a(0, L)
+    a0 = a(x, L, 0)
     sum = np.zeros(np.size(x))
     for i in np.arange(1, n + 1):
-        sum += ((a(i, L) * np.cos((i * np.pi * x) / L)) + (b(i, L) * np.sin((i * np.pi * x) / L)))
+        sum += ((a(x, L, i) * np.cos((i * np.pi * x) / L)) + (b(x, L, i) * np.sin((i * np.pi * x) / L)))
     return (a0 / 2) + sum   
 
-plt.plot(x, f(x)) 
-plt.plot(x, Sf(x, L), '.', color = 'red') 
-plt.show() 
+if __name__ == '__main__':
+
+	N = 1000
+	x = np.linspace(0, 10, N)
+	T = 2
+	L = T / 2 
+
+	''' Test Function '''
+	def f(x): 
+	    a = np.sin((np.pi) * x)
+	    a = np.where(a > 0, 1, 0)
+	    return a
+
+	plt.plot(x, f(x)) 
+	plt.plot(x, Sf(x, L, 6), color = 'red') 
+	plt.show() 
