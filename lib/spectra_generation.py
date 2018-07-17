108c1a013500e69754b781007b736cadc5e2295b
mckib2 <nicholas.bgp@gmail.com>
2018-07-17 14:19:57 -0600

    Split run into subfunctions.

--- lib/spectra_generation.py
+++ lib/spectra_generation.py
@@ -40,6 +40,73 @@ def loader(directory='/home/nicholas/Documents/mri-ssfp-matlab/lib/spectral_prof
 
     return(kspace)
 
+def SSFP_data_formatter(alpha,Ns):
+    return({
+        'M0': 1.0,
+        'alpha': alpha,
+        'phi': 0.0,
+        'Nr': 200,
+        'T1': 800.0/1000.0,
+        'T2': 200.0/1000.0,
+        'Ns': Ns,
+        'f0': 0,
+        'f1': 166
+    })
+
+def gen_sim_basis(args,TEs,dphis):
+    sim_basis = np.zeros([ args['Ns'],len(TEs)*len(dphis)+1 ],dtype='complex')
+    idx = 1
+    for TE in TEs:
+        for dphi in dphis:
+            f,Mc = ssfp.SSFP_SpectrumF(**args,TR=2*TE,TE=TE,dphi=dphi)
+            # Tuck Mc away
+            sim_basis[:,idx] = Mc
+            idx += 1
+    sim_basis[:,0] = sim_basis[:,0] + np.mean(np.mean(np.absolute(sim_basis[:,1:]),axis=1))
+    return(sim_basis)
+
+def solve_coeffs(sim_basis,f):
+    c = np.linalg.lstsq(sim_basis,f,rcond=None)[0]
+    approx = sim_basis.dot(c)
+    return(c,approx)
+
+def apply_coeffs(kSpace,c):
+    res = np.zeros([ kSpace.shape[0],kSpace.shape[1],kSpace.shape[3] ],dtype='complex')
+    for coil in range(kSpace.shape[3]):
+        for ii in range(kSpace.shape[4]):
+            data = np.squeeze(kSpace[:,:,:,:,ii])
+
+            num_avgs = data.shape[2]
+            avg = (np.squeeze(np.sum(data,axis=2))/num_avgs)[:,:,coil]
+            imData = np.fft.ifftshift(np.fft.ifft2(avg))
+
+            # Put together the image
+            res[:,:,coil] += imData*c[ii]
+    res = np.sum(res**2,axis=2)
+    return(res)
+
+
+def plotter(sim_basis,f,approx,res):
+    # Show the simulated basis
+    plt.subplot(2,1,1)
+    plt.plot(np.absolute(sim_basis))
+    plt.title('Simulated Basis')
+
+    plt.subplot(2,1,2)
+    plt.plot(f)
+    plt.show()
+
+    # check out how well we did
+    plt.plot(np.absolute(approx))
+    plt.title('Function Approximation')
+    plt.show()
+
+    plt.imshow(np.abs(res),cmap='gray')
+    plt.show()
+
+    plt.plot(np.abs(res[:,int(res.shape[1]/2)]))
+    plt.show()
+
 def run(kSpace,
         width=150,
         offset=None,
@@ -52,66 +119,25 @@ def run(kSpace,
         offset = int(Ns/2)
 
     # Arguments for ssfp.SSFP_SpectrumF()
-    args = {
-        'M0': 1.0,
-        'alpha': alpha,
-        'phi': 0.0,
-        'Nr': 200,
-        'T1': 800.0/1000.0,
-        'T2': 200.0/1000.0,
-        'Ns': Ns,
-        'f0': 0,
-        'f1': 166
-    }
+    args = SSFP_data_formatter(alpha,Ns)
 
     # Generate the basis
-    sim_basis = np.zeros([ args['Ns'],len(TEs)*len(dphis)+1 ],dtype='complex')
-    idx = 1
-    for TE in TEs:
-        for dphi in dphis:
-            f,Mc = ssfp.SSFP_SpectrumF(**args,TR=2*TE,TE=TE,dphi=dphi)
-            # Tuck Mc away
-            sim_basis[:,idx] = Mc
-            idx += 1
-
-    sim_basis[:,0] = sim_basis[:,0] + np.mean(np.mean(np.absolute(sim_basis[:,1:]),axis=1))
-    plt.subplot(2,1,1)
-    plt.plot(np.absolute(sim_basis))
-    plt.title('Simulated Basis')
+    sim_basis = gen_sim_basis(args,TEs,dphis)
 
     # Get a filter
     f = get_func(Ns=Ns,width=width,offset=offset)
-    plt.subplot(2,1,2)
-    plt.plot(f)
 
     # solve for coefficients and approximate the function
-    c = np.linalg.lstsq(sim_basis,f,rcond=None)[0]
-    approx = sim_basis.dot(c)
-
-    # check out how well we did
-    plt.plot(np.absolute(approx))
-    plt.title('Function Approximation')
-    plt.show()
+    c,approx = solve_coeffs(sim_basis,f)
 
     # Now apply the coefficients to the images
-    res = np.zeros([ kSpace.shape[0],kSpace.shape[1],kSpace.shape[3] ],dtype='complex')
-    for coil in range(kSpace.shape[3]):
-        for ii in range(kSpace.shape[4]):
-            data = np.squeeze(kSpace[:,:,:,:,ii])
-
-            num_avgs = data.shape[2]
-            avg = (np.squeeze(np.sum(data,axis=2))/num_avgs)[:,:,coil]
-            imData = np.fft.ifftshift(np.fft.ifft2(avg))
-
-            # Put together the image
-            res[:,:,coil] += imData*c[ii]
-
-    final_res = np.sum(res**2,axis=2)
-    plt.imshow(np.abs(final_res),cmap='gray')
-    plt.show()
+    res = apply_coeffs(kSpace,c)
 
     # You really need to to a t1, t2 map to use the simulated data, otherwise use
     # measured data or use a dictionary approach ala MRF
+
+    # Show me the money
+    plotter(sim_basis,f,approx,res)
 
 if __name__ == '__main__':
     # Load in all our data
